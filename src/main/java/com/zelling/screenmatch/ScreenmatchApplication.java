76b3e90f20358df2bd6d906daecc98da7116b171
FrancyelZelling <zellingf@gmail.com>
2024-11-24 12:39:34 -0300

    backup

--- src/main/java/com/zelling/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/zelling/screenmatch/ScreenmatchApplication.java
@@ -1,47 +1,21 @@
 package com.zelling.screenmatch;
 
-import com.zelling.screenmatch.model.DadosEpisodio;
-import com.zelling.screenmatch.model.DadosSerie;
-import com.zelling.screenmatch.model.DadosTemporada;
-import com.zelling.screenmatch.service.ConsumoApi;
-import com.zelling.screenmatch.service.ConverteDados;
+import com.zelling.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		var api = new ConsumoApi();
-		var conversor = new ConverteDados();
-		// pega dados de uma serie
-		var json = api.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=e4e7f21a");
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
 
-		// pega dados de um episodio
-		json = api.obterDados("http://www.omdbapi.com/?t=gilmore+girls&Season=1&Episode=2&apikey=e4e7f21a");
-		DadosEpisodio dadosEps = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEps);
-
-		List<DadosTemporada> listaTemporadas = new ArrayList<>();
-
-		// pega dados de todas temporadas
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = api.obterDados("http://www.omdbapi.com/?t=gilmore+girls&Season=" + i + "&apikey=e4e7f21a");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			listaTemporadas.add(dadosTemporada);
-		}
-
-		System.out.println(listaTemporadas);
-	}
+        principal.exibeMenu();
+    }
 }
--- src/main/java/com/zelling/screenmatch/principal/Principal.java
+++ src/main/java/com/zelling/screenmatch/principal/Principal.java
@@ -1,14 +1,39 @@
 package com.zelling.screenmatch.principal;
 
+import com.zelling.screenmatch.model.DadosEpisodio;
+import com.zelling.screenmatch.model.DadosSerie;
+import com.zelling.screenmatch.model.DadosTemporada;
+import com.zelling.screenmatch.service.ConsumoApi;
+import com.zelling.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner scanner = new Scanner(System.in);
+    private ConsumoApi api = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "http://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=e4e7f21a";
 
     public void exibeMenu(){
         System.out.println("Digite a série para busca");
         String nomeSerie = scanner.nextLine();
+        String json = api.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+        List<DadosTemporada> listaTemporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+            json = api.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            listaTemporadas.add(dadosTemporada);
+        }
+
+        listaTemporadas.forEach(System.out::println);
+
+        listaTemporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
